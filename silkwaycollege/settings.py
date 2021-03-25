644c60ae8b282fb4cc3c0dfc539b4dbee1f93bed
Ваше Имя <you@example.com>
2021-03-25 12:57:22 +0600

    a

--- silkwaycollege/settings.py
+++ silkwaycollege/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8rp&j5jfqdi-spuqv-)!g%erfdw2qzizrp_h0keouv$0+391f('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'silkwaycollege.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if DEBUG:
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'college',
+            'USER': 'bbb',
+            'PASSWORD': 'baguvix123F',
+            'HOST': 'localhost',
+            'PORT':''
         }
     }
 else:
