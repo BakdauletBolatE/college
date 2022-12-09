550b11a819e701fc05a2d82f8b91090c57e2932a
Lisa <bakosh21345@gmail.com>
2022-12-09 17:39:43 +0600

    new temp

--- page/admin.py
+++ page/admin.py
@@ -1,5 +1,13 @@
 from page.models import Page
 from django.contrib import admin
 
+
+
+class PageAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'order', 'parent')
+    list_editable = ('order', 'parent')
+
+
 admin.site.register(Page)
 
