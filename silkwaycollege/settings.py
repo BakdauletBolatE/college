5350670e1cf000845db44b0bec81fbd187028dfc
Lisa <bakosh21345@gmail.com>
2022-12-19 16:31:00 +0600

    gramatic

--- silkwaycollege/settings.py
+++ silkwaycollege/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", default='asdasfsg')
 
-DEBUG = os.environ.get('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['*']
 
